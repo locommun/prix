dca3a0e5ce9fec4155c79298395f359b10bf07df
mxm <mxm@zedat.fu-berlin.de>
2012-11-09 14:50:58 +0100

    default location of templates is $app/templates

--- project/settings.py
+++ project/settings.py
@@ -110,9 +110,6 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     
-    # Why must this be absult ??
-    "/home/flyover/Workspace/prix/templates"
-    
 )
 
 INSTALLED_APPS = (
