e1f6163179e66d8d84aed151a188c3e189521bdf
Tobias Schmid <2toschmid@googlemail.com>
2012-11-11 20:05:28 +0100

    comments embedded

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,81 +1,20 @@
 class CommentsController < ApplicationController
-  # GET /Comments
-  # GET /Comments.json
-  def index
-   redirect_to root_path
-  end
-
-  # GET /Comments/1
-  # GET /Comments/1.json
-  def show
-    @comment = Comment.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @comment }
-    end
-  end
 
   # GET /Comments/new
   # GET /Comments/new.json
-  def new
-    @comment = Comment.new
-    @comment.announcement_id = params[:announcement_id] 
+  #def new
+  #  @comment = @parent.comments.build
+  #end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @comment }
-    end
-  end
-
-  # GET /Comments/1/edit
-  def edit
-    @comment = Comment.find(params[:id])
-  end
-
-  # POST /Comments
-  # POST /Comments.json
   def create
+    #@parent = Announcement.find_by_id(params[:announcement_id])
     @comment = Comment.new(params[:comment])
     if current_user
-     @comment.user = current_user
+      @comment.user = current_user
     end
     
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment.announcement, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment.save
+    redirect_to announcement_path(@comment.announcement)
   end
 
-  # PUT /Comments/1
-  # PUT /Comments/1.json
-  def update
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment.announcement, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /Comments/1
-  # DELETE /Comments/1.json
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to Comments_url }
-      format.json { head :no_content }
-    end
-  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   attr_accessible :text, :announcement_id, :user
   belongs_to :announcement
   belongs_to :user
-
+  validates_presence_of :text
 end
