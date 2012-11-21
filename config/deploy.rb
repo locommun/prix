a1232b9bf7811c56b2991a8f2197b2c3aafd8206
mxm <mxm@zedat.fu-berlin.de>
2012-11-21 12:36:15 +0100

    changes to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,16 +4,19 @@ set :scm, "git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
+set :user, "deploy"
+set :use_sudo, false
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "demestoa.spline.de"                          # Your HTTP server, Apache/etc
 role :app, "demestoa.spline.de"                          # This may be the same as your `Web` server
-role :db,  "127.0.0.1", :primary => true # This is where Rails migrations will run
+#role :db,  "127.0.0.1", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 #
-set :deploy_to, "/home/deploy/#{application}#"
+set :deploy_to, "/home/deploy/#{application}"
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
 # if you want to clean up old releases on each deploy uncomment this:
