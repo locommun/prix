eee95d21f1bc2f5dfa5fbcf9c903960339133885
andreasknoepfle <andreas.knoepfle@googlemail.com>
2012-12-03 23:30:34 +0100

    Merged tracking columns 4 analysis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128102145) do
+ActiveRecord::Schema.define(:version => 20121203220925) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -84,10 +84,9 @@ ActiveRecord::Schema.define(:version => 20121128102145) do
 
   create_table "user_trackings", :force => true do |t|
     t.string   "url"
-    t.string   "ip"
-    t.string   "useragent"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.text     "visitor",    :limit => 255
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "users", :force => true do |t|
