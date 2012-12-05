f8a0e5b284478898a2ee5383aaf916c3d7950f07
Tobias Schmid <2toschmid@googlemail.com>
2012-12-05 12:50:45 +0100

    godfather

--- app/models/dialog.rb
+++ app/models/dialog.rb
@@ -1,6 +1,7 @@
 class Dialog < ActiveRecord::Base
   attr_accessible :billboard_id, :godfather_id, :text, :user_id, :billboard, :user
   
+  belongs_to :godfather ,:class_name => 'User', :foreign_key => 'godfather_id'
   
   belongs_to :billboard
   belongs_to :user
